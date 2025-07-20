2a3e22fbac788340f0b4f65cb3843c32185bdbb6
HamzaHZA <hamza60991@gmail.com>
2025-07-20 13:39:15 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,18 @@
+a=input("enter first number =");
+b=input("second Number =");
+a=float(a)
+b=float(b)
+print("the first number a is =",a);
+print("second number b is =",b);
+c=a+b;
+c=float(c)
+print("the sum of the to number a and b is =",c);
+c=a-b;
+print("the substracton of two number a and b is =",c);
+c=a*b;
+print("the multiplication of two number a and b is =",c);
+c=a/b;
+print("the division of two number a and b is =",c)
+
+
+
