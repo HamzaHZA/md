007abdaa5dd4e8a7293fc488bb45034cbe5a0e97
HamzaHZA <hamza60991@gmail.com>
2025-07-20 13:46:27 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,7 @@
+a=input("enter the first name =")
+b=input("enter the second name =")
+print("the first name is = ",a)
+print("the second name is = ",b)
+c=a+b;
+print("Hello,",c,"Welcome to the python programe.")
+
