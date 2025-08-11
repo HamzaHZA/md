49366ed0d48656f9cde0cf535290d087448f67dc
HamzaHZA <hamza60991@gmail.com>
2025-08-11 18:07:29 +0530

    Add files via upload

---
+++ assigment3task1.py
@@ -0,0 +1,12 @@
+
+def factorial(n):
+    if n<2:
+       return 1
+    else:
+        return n*factorial(n-1)
+
+result=factorial(5)
+print('enter number is:',5)
+print("factorial of 5 is:",result)
+
+
